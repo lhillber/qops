7041009ce916c554fdfeb1cf01a6ad82e28c9761
Logan Hillberry <lhillberry@gmail.com>
2016-06-13 18:55:20 -0600

    support bi-partite entropies

--- matrix.py
+++ matrix.py
@@ -13,7 +13,8 @@ from functools import reduce
 import numpy as np
 import scipy.sparse as sps
 
-# Apply k-site-wide op to a list of k sites (js) corresponindg to the
+
+# apply k-site-wide op to a list of k sites (js) corresponindg to the
 # state-vector state.  ds is a list of local dimensions for each site of state,
 # assumed to be a lattice of qubits if not provided.
 # -------------------------------------------------------------------------------
@@ -23,16 +24,26 @@ def op_on_state(op, js, state, ds = None):
         ds = [2]*L
     else:
         L = len(ds)
-
     dn = np.prod(np.array(ds).take(js))
     dL = np.prod(ds)
     rest = np.setdiff1d(np.arange(L), js)
     ordering = list(rest) + list(js)
-
     state = state.reshape(ds).transpose(ordering)\
             .reshape(int(dL/dn), dn).dot(op).reshape(ds)\
             .transpose(np.argsort(ordering)).reshape(dL)
     return state
+
+
+# mememory intensive method, builds full rank local operator
+# ----------------------------------------------------------
+def op_on_state_2(local_op_list, js, state):
+    L = int( log(len(state), 2) )
+    I_list = [np.eye(2.0, dtype=complex)]*L
+    for j, local_op in zip(js, local_op_list):
+        I_list[j] = local_op
+    big_op = listkron(I_list)
+    return big_op.dot(state)
+
 
 # partial trace of a state vector, js are the site indicies kept
 # --------------------------------------------------------------
@@ -57,8 +68,10 @@ def rdms(state, js, ds=None):
             RDM[j, i] = np.conj(Rij)
     return RDM
 
-# partial trace of a  density matrix
-# ----------------------------------
+
+# TODO: BROKEN! fix it.
+# partial trace of density matrix rho, keeping sites with indices js
+# ------------------------------------------------------------------
 def rdmr(rho, js):
     print()
     L = int(log(len(rho), 2))
@@ -69,6 +82,7 @@ def rdmr(rho, js):
     rest = np.setdiff1d(np.arange(L), js)
     ordering = js+list(rest)
     orderingout = [o + L for o in ordering]
+
     ordering = ordering + orderingout
     block = rho.reshape(([2]*d)).transpose(ordering).reshape(2**(d-2*n), 2**n, 2**n)
     print(block)
@@ -76,35 +90,88 @@ def rdmr(rho, js):
     return RDM/np.trace(RDM)
 
 
-# mememory intensive method (oldest version, V0)
-# ----------------------------------------------
-def big_mat(local_op_list, js, state):
-    L = int( log(len(state), 2) )
-    I_list = [np.eye(2.0, dtype=complex)]*L
-    for j, local_op in zip(js, local_op_list):
-        I_list[j] = local_op
-    big_op = listkron(I_list)
-    return big_op.dot(state)
+# partial trace of density marix rho, removing the first site
+# -----------------------------------------------------------
+def traceout_first(rho, ld=2):
+    L = int(log(len(rho), 2))
+    dim = ld**(L - 1)
+    rho2L = np.zeros((dim, dim), dtype=rho.dtype)
+    rho = np.reshape(np.transpose(np.reshape(rho, (ld, dim, ld, dim)),
+                                  (1, 3, 0, 2)), (dim, dim, ld**2))
+    mask = np.cumsum((ld + 1) * np.ones(ld, dtype=int)) - (ld + 1)
+    for kk in range(dim):
+        for jj in range(dim):
+            rho2L[kk, jj] = np.sum(rho[kk, jj, mask])
+    return rho2L
+
+
+# partial trace of density marix rho, removing the last site
+# ----------------------------------------------------------
+def traceout_last(rho, ld=2):
+    L = int(log(len(rho), 2))
+    dim = ld**(L - 1)
+    rho1n = np.zeros((dim, dim), dtype=rho.dtype)
+    rho = np.reshape(np.transpose(np.reshape(rho, (dim, ld, dim, ld)),
+                                  (0, 2, 1, 3)), (dim, dim, ld**2))
+    mask = np.cumsum((ld + 1) * np.ones(ld, dtype=int)) - (ld + 1)
+    for kk in range(dim):
+        for jj in range(dim):
+            rho1n[kk, jj] = np.sum(rho[kk, jj, mask])
+    return rho1n
+
 
 # Hermitian conjugate
 # -------------------
-def dagger(mat):
-    return mat.conj().transpose()
+def dagger(rho):
+    return np.transpose(np.conj(rho))
+
+
+# check density matrix has trace one
+# ----------------------------------
+def istrace_one(rho):
+    tr = np.trace(rho)
+    if tr == 1.0:
+        return True
+    else:
+        return False
+
+
+# check density matrix is Hermitian
+# ---------------------------------
+def isherm(rho):
+    if rho == mx.dagger(rho):
+        return True
+    else:
+        return False
+
+
+# check density matrix is positive
+# --------------------------------
+def ispos(rho):
+    evals = sp.linalg.eigvalsh(rho)
+    if np.all(evals >= 0.0):
+        return True
+    else:
+        return False
+
 
 # Kroeneker product list of matrices
 # ----------------------------------
 def listkron(matlist):
     return reduce(lambda A,B: np.kron(A, B), matlist)
 
+
 # Kroeneker product list of sparse matrices
 # -----------------------------------------
 def spmatkron(matlist):
     return sps.csc_matrix(reduce(lambda A, B: sps.kron(A,B,'csc'),matlist)) 
 
+
 # dot product list of matrices
 # ----------------------------
 def listdot(matlist):
     return reduce(lambda A, B: np.dot(A, B), matlist)
+
 
 # replace small elements in an array
 # ----------------------------------
@@ -114,12 +181,14 @@ def edit_small_vals(mat, tol=1e-14, replacement=0.0):
     mat[mat<=tol] = replacement
     return mat
 
+
 # concatinate two dictionaries (second arg replaces first if keys in common)
 # --------------------------------------------------------------------------
 def concat_dicts(d1, d2):
     d = d1.copy()
     d.update(d2)
     return d
+
 
 # concatinate a list of dictionaries
 # ----------------------------------
@@ -132,8 +201,9 @@ def listdicts(dictlist):
 def dec_to_bin(n, count):
      return [(n >> y) & 1 for y in range(count-1, -1, -1)]
 
-# sparse matrix tensor product (custom just for fun)
-# --------------------------------------------------
+
+# sparse matrix tensor product (custom, just for fun)
+# ---------------------------------------------------
 def tensor(A, B):
     a_nrows, a_ncols = A.shape
     b_nrows, b_ncols = B.shape
@@ -149,7 +219,6 @@ def tensor(A, B):
     return M
 
 
-
 if __name__ == '__main__':
 
     import simulation.states as ss
@@ -157,7 +226,7 @@ if __name__ == '__main__':
     L = 7
     IC = 'f0-3-4_t90-p90'
 
-    js = [0,3,2]
+    js = [0,2,3]
     op = listkron( [ss.ops['X']]*(len(js)-1) + [ss.ops['H']] ) 
 
     print()
---
+++ measures.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-15 -*-
+
+# ==============================================================
+# Basic quantum measures
+#
+# by Logan Hillberry
+# ===============================================================
+
+from math import log
+import numpy as np
+import numpy as np
+import scipy as sp
+import scipy.linalg
+import matrix as mx
+
+
+# von Neumann entropy of reduced density matrix rho
+# -------------------------------------------------
+def vn_entropy(rho, tol=1e-14, get_snum=False):
+    # define 0 log 0 = 0 where 0 = tol
+    evals = sp.linalg.eigvalsh(rho)
+    s = -sum(el*log(el, 2) if el >= tol else 0.0  for el in evals)
+    # option for schmidt rank (number of non zero evals of rho)
+    if get_snum:
+        snum = sum(el > tol for el in evals)
+        return s, snum
+    else:
+        return s
+
+
+# expectation value of operator A w.r.t state
+# -------------------------------------------
+def exp_val(state, A):
+    if len(state.shape) == 2:
+        # input is a density matrix
+        exp_val = np.real(np.trace(state.dot(A)))
+    elif len(state.shape) == 1:
+        # input is a state vector
+        exp_val = np.real(np.conj(state).dot(A.dot(state))))
+    else:
+        print('input state not understood')
+        raise
+    return exp_val
+
+
+# get list of local density matrices
+# ----------------------------------
+def get_local_rhos(state):
+    L = int(log(len(state), 2))
+    local_rhos = np.asarray([mx.rdms(state, j) for j in range(L)])
+    return local_rhos
+
+
+# get list of bi-partition density matrices
+# -----------------------------------------
+def get_bipartition_rhos(state):
+    N = int(log(len(state), 2)) - 1
+    c = int(N/2)
+    left_rdms  = [0] * c
+    left_rdm = mx.rdms(state, range(c))
+    left_rdms[-1] = left_rdm
+    right_rdms = [0] * (N - c)
+    right_rdm = mx.rdms(state, range(c+1, N+1))
+    right_rdms[0] = right_rdm
+    for j in range(c-1):
+        left_rdm = mx.traceout_last(left_rdm)
+        left_rdms[c-j-2] = left_rdm
+        right_rdm = mx.traceout_first(right_rdm)
+        right_rdms[j+1] = right_rdm
+    if N % 2 != 0:
+        right_rdm = mx.traceout_first(right_rdm)
+        right_rdms[-1] = right_rdm
+    return left_rdms + right_rdms
+
+
+# get list of local expectation values
+# ------------------------------------
+def get_local_exp_vals(state, A):
+    local_rhos = get_local_rhos(state)
+    local_exp_vals = np.asarray(
+            [exp_val(rho) for rho in local_rhos]
+        )
+    return local_exp_vals
+
+
+# get list of local von Neumann entropies
+# ---------------------------------------
+def get_local_entopies(state):
+    local_rhos = get_local_rhos(state)
+    local_s = np.asarray([vn_entropy(rho) for rho in local_rhos])
+    return local_s
+
+
+# get list of bi-partition von Neumann entropies
+# ----------------------------------------------
+def get_bipartition_entropies(state):
+    bipartition_rhos = get_bipartition_rhos(state)
+    bipart_s = np.asarray([vn_entropy(rho) for rho in bipartition_rhos])
+    return bipart_s
